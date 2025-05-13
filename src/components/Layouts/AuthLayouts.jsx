46819f0f11801007128636985b19d59c5bcb9de6
anqalah <evodark7@gmail.com>
2025-05-13 14:15:02 +0800

    test logo

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../../../public/logo";
+import Logo from "../../../public/logo/logo.png";
 
 const AuthLayout = ({ children, title, type }) => {
   return (
