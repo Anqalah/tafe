2f3f94a3fc6a170be8052b3b2f1df5f91dc947c7
anqalah <evodark7@gmail.com>
2025-11-11 18:36:00 +0800

    cors lagi

--- src/config/axiosFastAPI.js
+++ src/config/axiosFastAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosFastAPI = axios.create({
-  baseURL: "http://https://taml.onrender.com", // FastAPI URL
+  baseURL: "https://taml.onrender.com", // FastAPI URL
   // baseURL: "http://localhost:5000", // FastAPI URL
   headers: {
     "Content-Type": "multipart/form-data",
